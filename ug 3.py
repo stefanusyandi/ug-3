baef95aa43e55261f52dc8c2338cd951fb691662
stefanusyandi <93316047+stefanusyandi@users.noreply.github.com>
2022-09-21 10:44:41 +0700

    Add files via upload

---
+++ ug 3.py
@@ -0,0 +1,48 @@
+kelas_tujuh = {
+    'nama_sekolah': 'SMP Kerten',
+    'alamat': 'Jl. aa no 3',
+    'tahun_ajaran': '2015/2016',
+    'kode_pos': '57132',
+    'kelas':
+    {
+        'kelas_a': 
+        {
+            'wali_kelas': 'Arifin',
+            'ketua_kelas': 'Budi',
+            'wakil_ketua_kelas': 'Dodi',
+            'bendahara': 'Ani',
+            'murid': ['Andi', 'Ani', 'Budi', 'Dodi', 'Yosiah'],
+            'nilai_ratarata': '85,7'
+        },
+        'kelas_b':
+        {
+            'wali_kelas': 'Boni',
+            'ketua_kelas': 'Yudi',
+            'wakil_ketua_kelas': 'Didit',
+            'bendahara': 'Sarah',
+            'murid': ['Udin', 'Sarah', 'Yudi', 'Didit', 'Maimunah'],
+            'nilai_ratarata': '80,3'
+           
+        },
+        'kelas_c':
+        {
+           'wali_kelas': 'Siswanto',
+            'ketua_kelas': 'Max',
+            'wakil_ketua_kelas': 'Nopal',
+            'bendahara': 'Dustin',
+            'murid': ['Michael', 'Dustin', 'Max', 'Nopal', 'Justin'],
+            'nilai_ratarata': '75,6'
+        },
+        'kelas_d':
+        {
+            'wali_kelas': 'Ningsih',
+            'ketua_kelas': 'Bondan',
+            'wakil_ketua_kelas': 'Boris',
+            'bendahara': 'Angel',
+            'murid': ['Heri', 'Angel', 'Bondan', 'Boris', 'Nando'],
+            'nilai_ratarata': '95,2'
+        }
+    }
+
+}
+print(kelas_tujuh)
